92aaf7c391480f3591c8bb858d378ebfb44a8c7c
pm72 <46081859+pm72@users.noreply.github.com>
2023-11-03 12:49:57 +0400

    Update function minus

--- first/nums.py
+++ first/nums.py
@@ -1,12 +1,17 @@
 def add(a, b):
   return a + b
 
-text = "New text"
 
 def minus(a, b):
   return a - b
 
 
+def mul(a, b):
+  return a * b
+
+
+# ======================
 if __name__ == '__main__':
     print(add(3, 4))
     print(minus(3, 4))
+    print(mul(3, 4))
